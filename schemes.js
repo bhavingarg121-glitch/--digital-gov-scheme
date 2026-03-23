803828456f5b77da34f7dd786b443a77e2a69810
Bhavin Garg <bhavingarg121@gmail.com>
2026-03-23 23:56:50 +0530

    Create schemes.js

---
+++ schemes.js
@@ -0,0 +1,20 @@
+const schemesIndex = [
+  {
+    name: "PM Kisan Yojana",
+    keywords: ["pm kisan", "farmer", "kisaan", "किसान"],
+    link: "https://pmkisan.gov.in/",
+    category: "Farmers"
+  },
+  {
+    name: "National Scholarship Portal",
+    keywords: ["scholarship", "student", "छात्रवृत्ति"],
+    link: "https://scholarships.gov.in/",
+    category: "Students"
+  },
+  {
+    name: "Pension Schemes",
+    keywords: ["pension", "middle class", "retirement", "पेंशन"],
+    link: "https://www.nsiindia.gov.in/",
+    category: "Middle Class"
+  }
+];
