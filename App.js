0fbfe71db678b2832dc85d888188f85ea23fb7f7
Bhavin Garg <bhavingarg121@gmail.com>
2026-04-03 16:37:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ScholarshipPage from "./ScholarshipPage";
 import PensionPage from "./PensionPage";
 import Dashboard from "./Dashboard";
@@ -8,6 +8,16 @@ import RegisterPage from "./RegisterPage";
 function App() {
   return (
     <Router>
+      {/* Navigation bar */}
+      <nav className="bg-gray-800 p-4 text-white flex gap-4">
+        <Link to="/dashboard" className="hover:underline">Dashboard</Link>
+        <Link to="/scholarship" className="hover:underline">Scholarship</Link>
+        <Link to="/pension" className="hover:underline">Pension</Link>
+        <Link to="/login" className="hover:underline">Login</Link>
+        <Link to="/register" className="hover:underline">Register</Link>
+      </nav>
+
+      {/* Routes */}
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
